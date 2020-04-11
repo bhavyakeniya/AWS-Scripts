70e84eb9c545da9476a75264d66df7ded06a3019
bhavyakeniya <58433686+bhavyakeniya@users.noreply.github.com>
2020-04-12 02:05:41 +0530

    Update delete_unused_ebs_volumes.py

--- delete_unused_ebs_volumes.py
+++ delete_unused_ebs_volumes.py
@@ -3,6 +3,7 @@
 # You can also assign a special tag for the volume, such as “DND” (Do Not Delete).
 # The script will filter out the volumes with the “DND” tag and delete the rest
 
+# Link to the blog for this: https://medium.com/@keniya.bhavya/how-to-detect-and-delete-unused-aws-ebs-volumes-using-a-lambda-function-and-cloudwatch-6a5fdb08c730?source=friends_link&sk=e8bad0ef2fbffd899f988d039b374a05
 
 #!/usr/bin/python
 # -*- coding: utf-8 -*-
@@ -18,12 +19,12 @@ def lambda_handler(event, context):
                 v = ec2.Volume(vol.id)
                 v.delete()
                 print 'Deleted ' + vid
-    continue
-    for tag in vol.tags:
-        if tag['Key'] == 'Name':
-            value = tag['Value']
-            if value != 'DND' and vol.state == 'available':
-                vid = vol.id
-                v = ec2.Volume(vol.id)
-                v.delete()
-                print 'Deleted ' + vid
+        continue
+        for tag in vol.tags:
+            if tag['Key'] == 'Name':
+                value = tag['Value']
+                if value != 'DND' and vol.state == 'available':
+                    vid = vol.id
+                    v = ec2.Volume(vol.id)
+                    v.delete()
+                    print 'Deleted ' + vid
