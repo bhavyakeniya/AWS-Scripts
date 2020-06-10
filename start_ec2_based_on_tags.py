4381c2afcc0364d773bf0e5b34902a3801737b0f
bhavyakeniya <58433686+bhavyakeniya@users.noreply.github.com>
2020-06-11 00:57:34 +0530

    Create start_ec2_based_on_tags.py

---
+++ start_ec2_based_on_tags.py
@@ -0,0 +1,23 @@
+# Lambda function to start ec2 instances that have a particular tag
+
+import boto3
+ec2 = boto3.resource('ec2')
+
+def lambda_handler(event, context):
+
+    filters = [{
+            'Name': 'tag:your-tag-name',
+            'Values': ['your-tag-value']
+        },
+        {
+            'Name': 'instance-state-name', 
+            'Values': ['stopped']
+        }
+    ]
+    
+    instances = ec2.instances.filter(Filters=filters)
+
+
+    for instance in instances:
+        print('starting', instance) #console log to Cloudwatch
+        #instance.start() # uncomment after testing 
