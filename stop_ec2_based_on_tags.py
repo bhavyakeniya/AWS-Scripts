1cae0f16d2b0fc02927b9ffaf349f54d3822f3fb
bhavyakeniya <58433686+bhavyakeniya@users.noreply.github.com>
2020-06-11 00:59:28 +0530

    Create stop_ec2_based_on_tags.py

---
+++ stop_ec2_based_on_tags.py
@@ -0,0 +1,23 @@
+# Stop ec2 instances that match a particular tag
+
+import boto3
+ec2 = boto3.resource('ec2')
+
+def lambda_handler(event, context):
+
+    filters = [{
+            'Name': 'tag:your-tag-name',
+            'Values': ['your-tag-value']
+        },
+        {
+            'Name': 'instance-state-name', 
+            'Values': ['running']
+        }
+    ]
+    
+    instances = ec2.instances.filter(Filters=filters)
+
+
+    for instance in instances:
+        print('stopping', instance) #console logging so you can see which instances were stopped in CloudWatch
+        #instance.stop() #uncomment this once you've tested
