bffed48b591ae3bb0e85d15c32c9cb432b2aa377
bhavyakeniya <58433686+bhavyakeniya@users.noreply.github.com>
2020-06-11 19:03:35 +0530

    Create terminate_e2_based_on_tags.py

---
+++ terminate_e2_based_on_tags.py
@@ -0,0 +1,21 @@
+import boto3
+ec2 = boto3.resource('ec2')
+
+def lambda_handler(event, context):
+
+    filters = [{
+            'Name': 'tag:your-tag-name',
+            'Values': ['your-tag-value']
+        },
+        {
+            'Name': 'instance-state-name', 
+            'Values': ['running']
+        }
+    ]
+    
+    instances = ec2.instances.filter(Filters=filters)
+
+
+    for instance in instances:
+        print('Terminating', instance) # log which instances terminate to CloudWatch
+        #instance.terminate() # uncomment when testing complete
